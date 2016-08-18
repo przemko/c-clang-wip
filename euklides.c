23fbe9120759c5a02298dfb3a1f2222c44016d17
Przemysław Kobylański <przemko@mac.com>
2016-08-18 17:09:57 +0200

    funkcja nwd

--- euklides.c
+++ euklides.c
@@ -6,6 +6,18 @@
 #include <stdio.h>
 #include <assert.h>
 
+int nwd(int a, int b)
+{
+  while(b > 0)
+  {
+    // niezmiennik: nwd(m, n) = nwd(a, b)
+    int reszta = a % b;
+    a = b;
+    b = reszta;
+  }
+  return a;
+}
+
 int main(void)
 {
   int m, n;
@@ -15,16 +27,7 @@ int main(void)
   printf("   Podaj drugą dodatnią liczbę całkowitą: ");
   scanf("%d", &n);
   assert(n > 0);
-  int a = m;
-  int b = n;
-  while(b > 0)
-  {
-    // niezmiennik: nwd(m, n) = nwd(a, b)
-    int reszta = a % b;
-    a = b;
-    b = reszta;
-  }
-  printf("nwd(%d, %d) = %d\n", m, n, a);
+  printf("nwd(%d, %d) = %d\n", m, n, nwd(m, n));
   return 0;
 }
 
