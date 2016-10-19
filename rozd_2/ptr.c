2f359d76df3c876643c959a1bb567ecf73aa105a
Przemysław Kobylański <przemko@mac.com>
2016-10-19 09:34:39 +0200

    poprawki

--- rozd_2/ptr.c
+++ rozd_2/ptr.c
@@ -1,4 +1,12 @@
 // ptr.c
+//
+// wynik analizy statycznej:
+//
+// $ clang --analyze ptr.c 
+// ptr.c:23:18: warning: Use of memory after it is freed
+//   printf("%d\n", *y); // ta instrukcja nie ma sensu
+//                  ^~
+// 1 warning generated.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +19,8 @@ int main(void)
   *x = 13;
   y = x;
   printf("%d\n", *y);
+  free(x);
+  printf("%d\n", *y); // ta instrukcja nie ma sensu
   return 0;
 }
 
