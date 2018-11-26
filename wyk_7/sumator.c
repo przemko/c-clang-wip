32f8f1c82cdb66b78a6e247b184b361fc951c293
Przemyslaw Kobylanski <przemko@mac.com>
2018-11-26 13:11:14 +0100

    poprawki

--- wyk_7/sumator.c
+++ wyk_7/sumator.c
@@ -3,8 +3,8 @@
 // Program czyta liczby rzeczywiste i oblicza ich sumę.
 // Jeśli nie podano parametru wywołania, to czyta ze standardowego wyjścia.
 // W przeciwnym przypadku czyta z pliku, którego nazwę podano.
-// Program kończy czytanie gdy skończy się strumień znaków lub nie może 
-// zinterpretować wejścia jako liczba rzeczywista.
+// Program kończy czytanie gdy skończy się strumień znaków lub nie może 
+// zinterpretować wejścia jako liczba rzeczywista.
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -23,16 +23,16 @@ int main(int argc, char **argv)
 	  return 1;
 	}
     }
-
+  
   double suma = 0.0;
   
   while(true)
     {
-      double wartość;
-      int wynik = fscanf(fp, "%lf", &wartość);
+      double wartosc;
+      int wynik = fscanf(fp, "%lf", &wartosc);
       if(wynik < 1)
 	break;
-      suma = suma + wartość;
+      suma = suma + wartosc;
     }
   fclose(fp);
 
