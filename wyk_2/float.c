fab30eb101862fdd5c4d45bdb39965e24f83eb66
Przemyslaw Kobylanski <przemko@mac.com>
2020-10-18 12:15:56 +0200

    dodano komentarze

--- wyk_2/float.c
+++ wyk_2/float.c
@@ -5,8 +5,8 @@
 int main(void)
 {
   float x;
-  scanf("%f", &x);
-  printf("%f\n", x);
+  scanf("%f", &x);   //      wpisz 1234.5678
+  printf("%f\n", x); // pojawi się 1234.567749
   return 0;
 }
 
