cdd62f225ecbff22e0ae5db634c5446831365df7
Przemyslaw Kobylanski <przemko@mac.com>
2020-10-22 11:36:02 +0200

    dodano przykład instrukcji goto

---
+++ wyk_3/goto.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int n;
+  printf("Podaj ile liczb wydrukować: ");
+  scanf("%d", &n);
+  printf("\n\n");
+  int i = 1;
+
+Pętla:
+
+  if(i > n)
+    goto Koniec;
+  printf("%d\n", i);
+  i = i + 1;
+  goto Pętla;
+
+Koniec:
+
+  return 0;
+}
+
