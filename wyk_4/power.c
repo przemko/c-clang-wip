8ca4e7161f4f1c3c797355ef7ca320ef87d6ce57
Przemyslaw Kobylanski <przemko@mac.com>
2020-10-27 16:58:26 +0100

    dodano przykład do wykładu czwartego

---
+++ wyk_4/power.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+int main(void)
+{
+  int x;
+  printf(" podaj dodatnie x: ");
+  scanf("%d", &x);
+  assert(x > 0);
+  int n;
+  printf("podaj nieujemne n: ");
+  scanf("%d", &n);
+  assert(n >= 0);
+
+  int y = 1;
+  int p = x;
+  int i = n;
+  while(i > 0)
+  {
+    assert (y * pow(p, i) == pow(x, n));
+    if(i % 2 == 0)
+    {
+      p = p * p;
+      i = i / 2;
+    }
+    else
+    {
+      y = y * p;
+      i = i - 1;
+    }
+  }
+
+  printf("%d ^ %d = %d\n", x, n, y);
+  return 0;
+}
+
