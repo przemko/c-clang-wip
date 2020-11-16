4262d3b31673c314ad24ba4daab0c66221ec9ef2
Przemyslaw Kobylanski <przemko@mac.com>
2020-11-16 09:22:59 +0100

    dodano przykład z unią

---
+++ wyk_5/uni.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct Mężczyzna
+{
+  char imię[20];
+  char nazwisko[20];
+};
+
+struct Kobieta
+{
+  char imię[20];
+  char nazwisko[20];
+  char nazwisko_panieńskie[20];
+};
+
+union Wariant
+{
+  struct Mężczyzna m;
+  struct Kobieta k;
+};
+
+struct Osoba
+{
+  bool czy_mężczyzna;
+  union Wariant w;
+};
+
+void drukuj (struct Osoba o)
+{
+  if(o.czy_mężczyzna)
+    printf("%s %s\n", o.w.m.imię, o.w.m.nazwisko);
+  else
+    printf("%s %s z domu %s\n", o.w.k.imię, o.w.k.nazwisko,
+                                o.w.k.nazwisko_panieńskie);
+}
+
+int main(void)
+{
+  struct Osoba pracownik;
+  pracownik.czy_mężczyzna = false;
+  strcpy(pracownik.w.k.imię,                "Anna");
+  strcpy(pracownik.w.k.nazwisko,            "Nowak");
+  strcpy(pracownik.w.k.nazwisko_panieńskie, "Kowalska");
+  drukuj(pracownik);
+  return 0;
+}
+
