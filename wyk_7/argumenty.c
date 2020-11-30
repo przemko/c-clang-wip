94e37ba01aa7eacce47c64752dd8a4a862c0924a
Przemyslaw Kobylanski <przemko@mac.com>
2020-11-30 06:06:21 +0100

    zmiana nazwy

---
+++ wyk_7/argumenty.c
@@ -0,0 +1,14 @@
+// argumenty.c
+//
+// Program drukuje argumenty uruchomienia programu.
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("Liczba argumentów (argc) = %d\n", argc);
+  printf("Kolejne argumenty o indeksach od 0 do %d:\n", argc - 1);
+  for(int i = 0; i < argc; i++)
+    printf("\targv[%d] = \"%s\"\n", i, argv[i]);
+  return 0;
+}
