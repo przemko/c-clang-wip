eaec1fdbed84516f546c95caf58082ef9dcff218
Przemyslaw Kobylanski <przemko@mac.com>
2020-12-06 21:02:33 +0100

    przesunięcie gwiazdek

--- wyk_8/drzewa/drzewa.h
+++ wyk_8/drzewa/drzewa.h
@@ -2,14 +2,14 @@
 
 struct Węzeł {
 	int wartość;
-	struct Węzeł *lewe;
-	struct Węzeł *prawe;
+	struct Węzeł* lewe;
+	struct Węzeł* prawe;
 };
 
-typedef struct Węzeł *Drzewo;
+typedef struct Węzeł* Drzewo;
 
 void drukuj_prefiksowo(Drzewo wsk);
 void drukuj_infiksowo(Drzewo wsk);
 void drukuj_postfiksowo(Drzewo wsk);
-void wstaw_uporzadkowane(Drzewo *wsk, int i);
-void usun(Drzewo *wsk);
+void wstaw_uporzadkowane(Drzewo* wsk, int i);
+void usun(Drzewo* wsk);
