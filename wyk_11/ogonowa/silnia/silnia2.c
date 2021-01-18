b08278636fda90e9e56cf47807ff432f7d2445e2
Przemyslaw Kobylanski <przemko@mac.com>
2021-01-18 10:03:47 +0100

    dodano komentarz

--- wyk_11/ogonowa/silnia/silnia2.c
+++ wyk_11/ogonowa/silnia/silnia2.c
@@ -11,3 +11,10 @@ int silnia(int n)
   return silnia_rek(n, 1);
 }
 
+// silnia_rek(5, 1) = silnia_rek(4, 5 * 1)
+//                  = silnia_rek(3, 4 * 5)
+//                  = silnia_rek(2, 3 * 20)
+//                  = silnia_rek(1, 2 * 60)
+//                  = silnia_rek(0, 1 * 120)
+//                  = 120
+
